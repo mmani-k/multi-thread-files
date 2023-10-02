9a55af9f0fbdcebdce6278af671eeb11e4f763e3
Mathew Mani <mmani.kak@gmail.com>
2023-10-02 10:11:56 -0500

    Update FileReader.py

--- FileReader.py
+++ FileReader.py
@@ -3,11 +3,11 @@ class FileReader:
 
     def read_the_file_in_chunks(self, file_name, chunk_bounds):
         """
-        Opening the file inT each thread so as to access the file
+        Opening the file in each thread so as to access the file
         handle in a thread-safe manner. This is very inefficient
-        in real life, but in AWS S3, there is support for accessing
+        in local file system, but in AWS S3, there is support for accessing
         ranges of files in a thread-safe manner. This application is
-        just a local replica of code that works in S3 for demo purpose
+        just a local demo replica of code that works in S3
         """
         file_name = file_name
         f = open(file_name, "r")
